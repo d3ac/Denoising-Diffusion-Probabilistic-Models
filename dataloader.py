5b99fa5dff40859dfcf45ceec1affb7a6a01f61a
d3ac <1048563083@qq.com>
2024-12-01 00:30:39 +0800

    add dataloader

---
+++ dataloader.py
@@ -0,0 +1,36 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+def get_transforms():
+    return transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+class spriteDataset(Dataset):
+    def __init__(self, sfilename, lfilename, transform, null_context=False):
+        self.sprites = np.load(sfilename)
+        self.slabels = np.load(lfilename)
+        print(f"sprite shape: {self.sprites.shape}")
+        print(f"labels shape: {self.slabels.shape}")
+        self.transform = transform
+        self.null_context = null_context
+        self.sprites_shape = self.sprites.shape
+        self.slabel_shape = self.slabels.shape
+
+    def __len__(self):
+        return len(self.sprites)
+
+    def __getitem__(self, idx):
+        if self.transform:
+            image = self.transform(self.sprites[idx])
+            if self.null_context:
+                label = torch.tensor(0).to(torch.int64)
+            else:
+                label = torch.tensor(self.slabels[idx]).to(torch.int64)
+        return (image, label)
+
+    def getshapes(self):
+        return self.sprites_shape, self.slabel_shape
